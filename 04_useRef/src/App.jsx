270368144638069d2a87783782d8ccdd071ff57b
kalpeshgoswami <kalpeshgoswami298@gmail.com>
2026-09-16 13:59:04 +0530

    add useRef example"

--- 04_useRef/src/App.jsx
+++ 04_useRef/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import UseRef from "./components/01_UseRef";
 import UseRef_02 from "./components/02_UseRef";
+import InputName from "./components/03_useRef";
 
 const App = () => {
 
@@ -14,7 +15,7 @@ const App = () => {
 
       {/* <UseRef_02/> */}
 
-      
+      <InputName/>
 
     </>
 
--- 04_useRef/src/components/03_useRef.jsx
+++ 04_useRef/src/components/03_useRef.jsx
@@ -4,12 +4,32 @@ const InputName = () => {
 
     const name = useRef("")
 
-    const handleSubmit =(e)=> {
+    const handleSubmit = (e) => {
         e.preventDefault()
 
-        console.log("name",name.current.value)
+        console.log("name", name.current.value)
     }
-   
-    
-    
+
+    console.log("it will not render again")
+
+
+    return (
+
+        <>
+
+            <form onSubmit={handleSubmit}>
+
+                <input type="text" placeholder="Enter your name" ref={name} />
+
+                <br />
+
+            <button type="submit">add</button>
+
+            </form>
+
+        </>
+
+    )
 }
+
+export default InputName
